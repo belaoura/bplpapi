537538baf5774faa64ee548fe261f2be8d68e2af
belaoura abdelwahab (bellegend) <bellegend@gmail.com>
2019-09-11 16:20:26 +0100

    fixed and styled the simple example of consuming the api with javascript

--- ConsumeApi.js
+++ ConsumeApi.js
@@ -17,55 +17,52 @@ function list(items, level) {
         }
     }
 }
-  document.addEventListener('DOMContentLoaded',function(){
-  document.getElementById('getMessage').onclick=function(){
 
-       
-       var req;
-       req=new XMLHttpRequest();
-       req.open("GET", 'http://phpapi.test/api/book/read.php',true);
-       req.send();
-      
-       req.onload=function(){
-       var json=JSON.parse(req.responseText);
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('getMessage').onclick = function () {
 
-       //limit data called
-       // var son =Object.keys(json).filter(function(val) {
-       //     return (val.id >= 4);
-       // });
-           var books =json.books;
-          // console.log(books);
+        var req;
+        req = new XMLHttpRequest();
+        req.open("GET", 'http://phpapi.test/api/book/read.php', true);
+        req.send();
 
-         //  console.log( list(books, 5));
+        req.onload = function () {
+            var json = JSON.parse(req.responseText);
 
-      var html = "";
-           for (var i = 0; i < books.length; i++) {
-               var book = books[i];
-               html += "<div class = 'cat'>";
-               html += "<strong>" + book.leader + "</strong>: " + book.fields[i] + "<br>";
-               html += "</div><br>";
+            var books = json.books;
+            console.log(books[0]["fields"]);
+            console.log(books[0]["fields"][0]);
+            console.log(books[0]["fields"][4]["200"]);
+            console.log(books[0]["fields"][4]["200"].subfields["0"]);
+            console.log(books[0]["fields"][4]["200"].subfields["0"].$a);
+            console.log(books[0]["fields"][4]["200"].subfields["1"].$d);
 
-               for (var j = 0; j < book.fields.length; j++) {
-                   var tags = books.fields[j];
-                   html += "<strong>" + tags['200'] + "</strong>: " + book.fields.length + "<br>";
-                   console.log(tags[j]);
-               }
-           }
-      //loop and display data
-      // books.forEach(function(val) {
-      //     var keys = Object.keys(val);
-      //
-      //     html += "<div class = 'cat'>";
-      //     html += "<strong>" + books.fields + "</strong>: " + val[key] + "<br>";
-      //         keys.forEach(function(key) {
-      //         html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
-      //         });
-      //     html += "</div><br>";
-      // });
+            var html = "";
+            for (var i = 0; i < books.length; i++) {
+                // console.log(books[i]["fields"][4]["200"].subfields["0"].$a);
+                var book = books[i];
+                html += "<div class='card col-md-12 mb-2' >";
+                html += "<div class='card-body'>";
+                html += "<h4><strong> <span class='badge badge-info'>ISBN :</span> " +books[i]["fields"][0]["010"].subfields["0"].$a+ "</strong></h4><br>";
+                html += "<div class='row'> <div class='col'>";
+                html += " <input class='form-control form-control-sm' type='text' value='" + books[i]["fields"][4]["200"].subfields["0"].$a + "'> <br>";
+                html += " <input class='form-control form-control-sm'  type='text' value='" + books[i]["fields"][5]["210"].subfields["1"].$c + "'> <br>";
+                html += " <input class='form-control form-control-sm'  type='text' value='" + books[i]["fields"][5]["210"].subfields["0"].$a + "'> <br>";
+                html += " <input class='form-control form-control-sm'  type='text' value='" + books[i]["fields"][5]["210"].subfields["2"].$d + "'> <br>";
+                html += "</div> <div class='col'>";
+                html += " <input class='form-control form-control-sm' type='text' value='" + books[i]["fields"][4]["200"].subfields["1"].$d + "'> <br>";
+                html += " <input class='form-control form-control-sm'  type='text' value='" + books[i]["fields"][7]["215"].subfields["0"].$a + "'> <br>";
+                html += " <input class='form-control form-control-sm'  type='text' value='" + books[i]["fields"][7]["215"].subfields["1"].$d + "'> <br>";
+                html += " <input class='form-control form-control-sm'  type='text' value='" + books[i]["fields"][7]["215"].subfields["2"].$c + "'> <br>";
+                html += "</div> </div>";
+                html += "</div>";
+                html += "</div>";
+            }
 
-      //append in message class
-      document.getElementsByClassName('message')[0].innerHTML=html;         
-      };
+
+            //append in message class
+            document.getElementsByClassName('message')[0].innerHTML = html;
+        };
     };
-  });
+});
   
